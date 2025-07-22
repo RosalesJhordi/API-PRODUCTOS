db118eada582ba2c11669882d97e5caf2bce8892
RosalesJhordi <1428060@senati.pe>
2025-07-21 20:21:40 -0500

    Comenzando el proyecto

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        // Busca o crea un usuario
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'google_id' => $googleUser->getId(),
+                'password' => bcrypt(Str::random(16)) // por si se requiere
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect('/home');
+    }
+}
